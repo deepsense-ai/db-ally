ce1c6b967e1ddadaf28d84d47cd92ceb5a33e111
Mateusz Hordyński <mateusz.hordynski@deepsense.ai>
2024-01-29 10:01:18 +0100

    Implement IQLParser

---
+++ src/dbally/iql/_exceptions.py
@@ -0,0 +1,35 @@
+import ast
+from typing import Union, Optional
+
+
+class IQLError(Exception):
+    """Base exception for all IQL parsing related exceptions."""
+
+    def __init__(self, message: str, node: Union[ast.stmt, ast.expr], source: str) -> None:
+        message = message + ": " + source[node.col_offset: node.end_col_offset]
+
+        super().__init__(message)
+        self.node = node
+        self.source = source
+
+
+class IQLArgumentParsingError(IQLError):
+    """Raised when an argument cannot be parsed into a valid IQL."""
+
+    def __init__(self, node: Union[ast.stmt, ast.expr], source: str) -> None:
+        message = f"Not a valid IQL argument"
+        super().__init__(message, node, source)
+
+
+class IQLUnsupportedSyntaxError(IQLError):
+    """Raised when trying to parse an unsupported syntax."""
+
+    def __init__(self, node: Union[ast.stmt, ast.expr], source: str, context: Optional[str] = None) -> None:
+        node_name = node.__class__.__name__
+
+        message = f"{node_name} syntax is not supported in IQL"
+
+        if context:
+            message += " " + context
+
+        super().__init__(message, node, source)
---
+++ src/dbally/iql/_parser.py
@@ -0,0 +1,70 @@
+import ast
+from typing import Union, Any
+
+from dbally.iql._exceptions import IQLArgumentParsingError, IQLUnsupportedSyntaxError, IQLError
+from dbally.iql._syntax import IQL
+
+
+class IQLParser:
+    """
+    Parses IQL string to tree structure.
+    """
+
+    def __init__(self, source: str):
+        self.source = source
+
+    def parse(self) -> IQL.Node:
+        """
+        Parse IQL string to root IQL.Node.
+
+        :return: IQL.Node which is root of the tree representing IQL query.
+        """
+        ast_tree = ast.parse(self.source)
+        first_element = ast_tree.body[0]
+
+        if not isinstance(first_element, ast.Expr):
+            raise IQLError("Not a valid IQL expression", first_element, self.source)
+
+        root = self._parse_node(first_element.value)
+        return root
+
+    def _parse_node(self, node: Union[ast.expr, ast.Expr]) -> IQL.Node:
+        if isinstance(node, ast.BoolOp):
+            return self._parse_bool_op(node)
+        elif isinstance(node, ast.UnaryOp) and isinstance(node.op, ast.Not):
+            return IQL.Not(self._parse_node(node.operand))
+        elif isinstance(node, ast.Call):
+            return self._parse_call(node)
+
+        raise IQLUnsupportedSyntaxError(node, self.source)
+
+    def _parse_bool_op(self, node: ast.BoolOp) -> IQL.BoolOp:
+        if isinstance(node.op, ast.Not):
+            return IQL.Not(self._parse_node(node.values[0]))
+        elif isinstance(node.op, ast.And):
+            return IQL.And([self._parse_node(x) for x in node.values])
+        elif isinstance(node.op, ast.Or):
+            return IQL.Or([self._parse_node(x) for x in node.values])
+        else:
+            raise IQLUnsupportedSyntaxError(node, self.source, context="BoolOp")
+
+    def _parse_call(self, node: ast.Call) -> IQL.FunctionCall:
+        func = node.func
+
+        if not isinstance(func, ast.Name):
+            raise IQLUnsupportedSyntaxError(node, self.source, context="FunctionCall")
+
+        args = []
+
+        for arg in node.args:
+            args.append(self._parse_arg(arg))
+
+        return IQL.FunctionCall(func.id, args)
+
+    def _parse_arg(self, arg: ast.expr) -> Any:
+        if isinstance(arg, ast.List):
+            return [self._parse_arg(x) for x in arg.elts]
+
+        if not isinstance(arg, ast.Constant):
+            raise IQLArgumentParsingError(arg, self.source)
+        return arg.value
