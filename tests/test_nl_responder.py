c5de7b5117789130d3e6e96ea2a5ae8ab1b9b4ba
alicja.kotyla <alicja.kotyla@deepsense.ai>
2024-02-26 20:59:29 +0100

    Add tests and fix requirements

---
+++ tests/test_nl_responder.py
@@ -0,0 +1,31 @@
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+
+from dbally.audit.event_tracker import EventTracker
+from dbally.nl_responder.nl_responder import NLResponder
+from dbally.data_models.answer import Answer
+
+
+@pytest.fixture
+def llm_client():
+    mock_client = Mock()
+    mock_client.text_generation = AsyncMock(return_value="db-ally is the best")
+    return mock_client
+
+
+@pytest.fixture
+def event_tracker():
+    return EventTracker()
+
+@pytest.fixture
+def answer():
+    return Answer(sql="Mock SQL", rows=[{"id": 1, "name": "Mock name"}])
+
+
+@pytest.mark.asyncio
+async def test_nl_responder(llm_client, answer, event_tracker):
+    nl_responder = NLResponder(llm_client)
+    
+    response = await nl_responder.generate_response(answer, "Mock question", event_tracker)
+    assert response == "db-ally is the best"
