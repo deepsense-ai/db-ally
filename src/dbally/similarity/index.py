7f8e3dd426e07e39b1bf5322a845fe28072cbbfd
Ludwik Trammer <ludwik.trammer@deepsense.ai>
2024-03-05 10:32:23 +0100

    Initial implementation of similarity index

---
+++ src/dbally/similarity/index.py
@@ -0,0 +1,63 @@
+import abc
+
+from dbally.similarity.fetcher import AbstractFetcher
+from dbally.similarity.store import AbstractStore
+
+
+class AbstractSimilarityIndex(metaclass=abc.ABCMeta):
+    """
+    Base class for all similarity indexes.
+
+    Usually it is recommended to use the SimilarityIndex class directly instead. AbstractSimilarityIndex may be used
+    to built custom index classes in more advanced cases when the clear separation of the store and the fetcher
+    is not a useful abstraction.
+    """
+
+    @abc.abstractmethod
+    def update(self) -> None:
+        """
+        Updates the store with the latest data.
+        """
+
+    @abc.abstractmethod
+    def similar(self, text: str) -> str:
+        """
+        Finds the most similar text or returns the original text if no similar text is found.
+
+        Args:
+            text: The text to find similar to.
+
+        Returns:
+            str: The most similar text or the original text if no similar text is found.
+        """
+
+
+class SimilarityIndex(AbstractSimilarityIndex):
+    """
+    Merges the store and the fetcher to provide a simple interface for keeping
+    the data store and the similairty store in sync and finding similar texts.
+    """
+
+    def __init__(self, store: AbstractStore, fetcher: AbstractFetcher):
+        self.store = store
+        self.fetcher = fetcher
+
+    def update(self) -> None:
+        """
+        Updates the store with the latest data from the fetcher.
+        """
+        data = self.fetcher.fetch()
+        self.store.store(data)
+
+    def similar(self, text: str) -> str:
+        """
+        Finds the most similar text in the store or returns the original text if no similar text is found.
+
+        Args:
+            text: The text to find similar to.
+
+        Returns:
+            str: The most similar text or the original text if no similar text is found.
+        """
+        found = self.store.find_similar(text)
+        return found if found else text
---
+++ src/dbally/similarity/sqlalchemy_base.py
@@ -0,0 +1,108 @@
+import abc
+from typing import List, Optional
+
+import sqlalchemy
+from sqlalchemy.sql.elements import ColumnClause
+
+from dbally.similarity.fetcher import AbstractFetcher
+from dbally.similarity.store import AbstractStore
+
+
+class SqlAlchemyFetcher(AbstractFetcher):
+    """
+    Fetches the data from the database using SQLAlchemy.
+    """
+
+    def __init__(self, sqlalchemy_engine: sqlalchemy.engine.Engine) -> None:
+        self.sqlalchemy_engine = sqlalchemy_engine
+
+    @abc.abstractmethod
+    def get_query(self) -> sqlalchemy.Select:
+        """
+        Returns query that will be used to fetch the data from the database. Should include a single text column.
+
+        Returns:
+            sqlalchemy.Select: The query to fetch the data.
+        """
+
+    def fetch(self) -> List[str]:
+        """
+        Fetches the data from the source and returns it as a list of strings.
+
+        Returns:
+            List[str]: The fetched data.
+        """
+        with self.sqlalchemy_engine.connect() as conn:
+            result = conn.execute(self.get_query())
+            return [row[0] for row in result]
+
+
+class SimpleSqlAlchemyFetcher(SqlAlchemyFetcher):
+    """
+    Fetches the data from a single column in the database.
+    """
+
+    def __init__(
+        self, sqlalchemy_engine: sqlalchemy.engine.Engine, column: ColumnClause, table: sqlalchemy.Table
+    ) -> None:
+        super().__init__(sqlalchemy_engine)
+        self.column = column
+        self.table = table
+
+    def get_query(self) -> sqlalchemy.Select:
+        """
+        Returns query that will be used to fetch the data from the database.
+
+        Returns:
+            sqlalchemy.Select: The query to fetch the data.
+        """
+        return sqlalchemy.select(self.column).select_from(self.table).distinct()
+
+
+class AbstractSqlAlchemyStore(AbstractStore, metaclass=abc.ABCMeta):
+    """
+    Stores the data in the database using SQLAlchemy.
+    """
+
+    def __init__(self, sqlalchemy_engine: sqlalchemy.engine.Engine, table_name: str, treshold: float = 0.8) -> None:
+        self.sqlalchemy_engine = sqlalchemy_engine
+        self.table_name = table_name
+        self.table = sqlalchemy.Table(table_name, sqlalchemy.MetaData(), sqlalchemy.Column("text", sqlalchemy.String))
+        self.treshold = treshold
+
+    def store(self, data: List[str]) -> None:
+        """
+        Stores the data. Should replace the previously stored data.
+
+        Args:
+            data: The data to store.
+        """
+        # Create the table if it doesn't exist
+        self.table.create(self.sqlalchemy_engine, checkfirst=True)
+
+        # Insert the data
+        with self.sqlalchemy_engine.connect() as conn:
+            conn.execute(self.table.delete())
+            conn.execute(self.table.insert(), [{"text": text} for text in data])
+            conn.commit()
+
+
+class CaseInsensitiveSqlAlchemyStore(AbstractSqlAlchemyStore):
+    """
+    Stores the data in the database using SQLAlchemy and defines "similarity" as case-insensitive equality.
+    """
+
+    def find_similar(self, text: str) -> Optional[str]:
+        """
+        Finds the text from the store that differs from the given text only in case.
+
+        Args:
+            text: The text to find similar to.
+
+        Returns:
+            Optional[str]: The most similar text or None if no similar text is found.
+        """
+        with self.sqlalchemy_engine.connect() as conn:
+            result = conn.execute(sqlalchemy.select(self.table.c.text).where(self.table.c.text.ilike(text)))
+            row = result.fetchone()
+            return row[0] if row else None
