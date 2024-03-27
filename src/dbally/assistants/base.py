f96c041ec25c31834595071a1c75dd31ee351c64
Sebastian Chwilczyński <sebastian.chwilczynski@deepsense.ai>
2024-03-27 13:02:09 +0000

    OpenAI assistants adapter

---
+++ src/dbally/assistants/base.py
@@ -0,0 +1,95 @@
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+from typing import Any, Dict, List
+
+
+class FunctionCallState(Enum):
+    """Enum to represent the state of the Assistants function call."""
+
+    SUCCESS = auto()
+    INVALID_ARGUMENTS = auto()
+    UNSUPPORTED_QUERY = auto()
+    INVALID_JSON = auto()
+
+    def __str__(self) -> str:
+        """Returns the string representation of the FunctionCallState. Used to give feedback to the LLM agent."""
+        return {
+            FunctionCallState.SUCCESS: "Success: The query was successfully processed by the dbally",
+            FunctionCallState.INVALID_ARGUMENTS: "ValueError: 'query' argument is required for the dballye",
+            FunctionCallState.UNSUPPORTED_QUERY: "UnsupportedQueryError: The query is not supported by the dbally",
+            FunctionCallState.INVALID_JSON: (
+                "ValueError: The JSON containing arguments for the function" "call could not be parsed with json.loads"
+            ),
+        }[self]
+
+
+class FunctionCallingError(Exception):
+    """Exception raised when an error occurs during Assistants function call function."""
+
+    def __init__(self, state: FunctionCallState, msg: str) -> None:
+        super().__init__(msg)
+        self.state = state
+
+
+class AssistantAdapter(ABC):
+    """This Interface is used to support dbally as a tool for Assistants API or
+    function calling. It is inspired by OpenAI function calling API, where user defines a
+    JSON definition of the function, and the LLM select when the function should be called.
+
+    The general idea is that we need to:
+    1. Create a custom instruction, describing when the Assistant should use dbally.
+    2. Generate a function JSON, compliant with provider API, describing how to use dbally.
+    3. Process the response from the Assistant, and return the dbally output.
+    """
+
+    @abstractmethod
+    def generate_instruction(
+        self,
+    ) -> str:
+        """This function should generate the instruction for the LLM, on when to use dbally.
+        Different LLMs may require different prompts for it.
+
+        Returns:
+            str: Instruction that should be provided to the LLM, so that it knows when to use dbally
+        """
+
+    @abstractmethod
+    def generate_function_json(
+        self,
+    ) -> Dict:
+        """Returns the definition of the function in JSON format. This JSON should be compliant with the provider API
+        and contain all the necessary information to execute the function correctly.
+
+        Returns:
+            Dict: JSON definition of the function
+        """
+
+    @abstractmethod
+    async def process_response(self, tool_calls: List[Any], raise_exception: bool = False) -> List[Any]:
+        """Process the response from the LLM, and return the dbally output. LLMs don't execute functions by themselves,
+        but set themselves in requires_action states, and then users need to execute functions by themselves.
+
+        Args:
+            tool_calls (List[Any]): The tool calls returned by the LLM
+            raise_exception (bool): Whether to raise an exception if an error occurs during function execution
+
+        Returns:
+            List[Any]: The output from the dbally engine or an empty list if dbally was not in tool_calls. The Any type
+            should be replaced with a dataclass specific to the LLM model containing the output of the function.
+
+        Raises:
+            FunctionCallingError: If an error occurs during function execution and `raise_exception` flag is set to True
+        """
+
+    @abstractmethod
+    def process_functions_execution(self, functions_executions: List[Any]) -> List[Dict[str, str]]:
+        """The result of process_response function may contain values that are not JSON serializable
+        or not compliant with the LLM provider API. This function should process the functions_executions
+        and return the values that are compliant with LLM provider API and can be sent back to the LLM.
+
+        Args:
+            functions_executions (List[Any]): Results of `process_response` function
+
+        Returns:
+            List[Dict[str, Any]]: Parsed functions_execution compliant with LLM provider API
+        """
---
+++ tests/unit/test_assistants_adapters.py
@@ -0,0 +1,107 @@
+import json
+from collections import namedtuple
+from typing import List
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+from openai.types.beta.threads.required_action_function_tool_call import Function, RequiredActionFunctionToolCall
+
+from dbally.assistants.base import FunctionCallingError, FunctionCallState
+from dbally.assistants.openai import _DBALLY_INFO, _DBALLY_INSTRUCTION, OpenAIAdapter, OpenAIDballyResponse
+from dbally.utils.errors import UnsupportedQueryError
+
+MOCK_VIEWS = {"view1": "description1", "view2": "description2"}
+F_ID = "f_id"
+
+
+@pytest.fixture
+def openai_adapter() -> OpenAIAdapter:
+    collection = AsyncMock()
+    collection.list = Mock(return_value=MOCK_VIEWS)
+    return OpenAIAdapter(collection)
+
+
+@pytest.fixture
+def tool_calls() -> List[RequiredActionFunctionToolCall]:
+    call = RequiredActionFunctionToolCall(
+        id=F_ID, function=Function(name="use_dbally", arguments='{"query": "mock query"}'), type="function"
+    )
+
+    return [call]
+
+
+def test_generate_instruction(openai_adapter):
+    k = list(MOCK_VIEWS.keys())
+    v = list(MOCK_VIEWS.values())
+    GT_INSTRUCTION = _DBALLY_INFO + f"{k[0]}: {v[0]}\n{k[1]}: {v[1]}\n" + _DBALLY_INSTRUCTION
+    assert openai_adapter.generate_instruction() == GT_INSTRUCTION
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("n_calls", [1, 3])
+async def test_process_response_no_query(n_calls, openai_adapter):
+    call = RequiredActionFunctionToolCall(
+        id=F_ID, function=Function(name="use_dbally", arguments="{}"), type="function"
+    )
+    tool_calls = [call] * n_calls
+
+    response = await openai_adapter.process_response(tool_calls)
+    state = FunctionCallState.INVALID_ARGUMENTS
+    assert response == [OpenAIDballyResponse(F_ID, state, str(state))] * n_calls
+
+
+@pytest.mark.asyncio
+async def test_process_response_no_query_exception(openai_adapter):
+    call = RequiredActionFunctionToolCall(
+        id=F_ID, function=Function(name="use_dbally", arguments="{}"), type="function"
+    )
+    tool_calls = [call]
+
+    with pytest.raises(FunctionCallingError) as excinfo:
+        await openai_adapter.process_response(tool_calls, raise_exception=True)
+
+    assert excinfo.value.state == FunctionCallState.INVALID_ARGUMENTS
+
+
+@pytest.mark.asyncio
+async def test_process_response_correct(openai_adapter, tool_calls):
+    Response = namedtuple("Response", ["results"])
+    MOCK_RESULT = Response("mock_results")
+    openai_adapter.collection.ask = AsyncMock(return_value=MOCK_RESULT)
+
+    response = await openai_adapter.process_response(tool_calls)
+    assert response == [OpenAIDballyResponse(F_ID, FunctionCallState.SUCCESS, json.dumps(MOCK_RESULT.results))]
+
+
+@pytest.mark.asyncio
+async def test_process_response_unsupported(openai_adapter, tool_calls):
+    openai_adapter.collection.ask = AsyncMock(side_effect=UnsupportedQueryError())
+
+    response = await openai_adapter.process_response(tool_calls)
+    state = FunctionCallState.UNSUPPORTED_QUERY
+    assert response == [OpenAIDballyResponse(F_ID, state, str(state))]
+
+
+@pytest.mark.asyncio
+async def test_process_response_unsupported_exception(openai_adapter, tool_calls):
+    openai_adapter.collection.ask = AsyncMock(side_effect=UnsupportedQueryError())
+
+    with pytest.raises(FunctionCallingError) as excinfo:
+        await openai_adapter.process_response(tool_calls, raise_exception=True)
+    assert excinfo.value.state == FunctionCallState.UNSUPPORTED_QUERY
+
+
+@pytest.mark.asyncio
+async def test_process_response_invalid_json(openai_adapter):
+    call = RequiredActionFunctionToolCall(
+        id=F_ID, function=Function(name="use_dbally", arguments="{invalid_json"), type="function"
+    )
+    tool_calls = [call]
+    response = await openai_adapter.process_response(tool_calls)
+    state = FunctionCallState.INVALID_JSON
+    assert response[0].state == state
+
+
+def test_process_functions_execution(openai_adapter):
+    response = [OpenAIDballyResponse(F_ID, FunctionCallState.SUCCESS, "mock_results")]
+    assert openai_adapter.process_functions_execution(response) == [{"tool_call_id": F_ID, "output": "mock_results"}]
