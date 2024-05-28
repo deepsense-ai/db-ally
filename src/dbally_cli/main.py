b54a2ddd2b3695439b3722a41ba8761b5e836dcd
Michał Pstrąg <michal.pstrag@icloud.com>
2024-05-28 12:32:10 +0200

    remove detector

--- src/dbally_cli/main.py
+++ src/dbally_cli/main.py
@@ -1,11 +1,8 @@
 import click
 
-from dbally_cli.similarity import update_index
-
 
 @click.group()
 def cli():
-    """Command line tool for interacting with Db-ally"""
-
-
-cli.add_command(update_index)
+    """
+    Command line tool for interacting with dbally.
+    """
