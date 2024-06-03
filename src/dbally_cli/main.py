70c8a1d5e7b3d49f29f80c63a7dce6e297157f0e
Michał Pstrąg <michal.pstrag@icloud.com>
2024-06-03 11:30:38 +0200

    rm gen

--- src/dbally_cli/main.py
+++ src/dbally_cli/main.py
@@ -1,13 +1,8 @@
 import click
 
-from dbally_cli.text2sql import generate_text2sql_view
-
 
 @click.group()
 def cli() -> None:
     """
     Command line tool for interacting with dbally.
     """
-
-
-cli.add_command(generate_text2sql_view)
