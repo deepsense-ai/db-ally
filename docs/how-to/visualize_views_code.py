849b4f7a878088c5f3b6b5ee4ff967655fef156c
Lukasz Karlowski <lukasz.karlowski@deepsense.ai>
2024-06-03 17:52:41 +0200

    Logger redirect

--- docs/how-to/visualize_views_code.py
+++ docs/how-to/visualize_views_code.py
@@ -2,12 +2,15 @@ import asyncio
 import dotenv
 import os
 
+import sqlalchemy
+
 import dbally
 from dbally.audit import CLIEventHandler
 from dbally.embeddings import LiteLLMEmbeddingClient
 from dbally.similarity import SimilarityIndex, SimpleSqlAlchemyFetcher, FaissStore
 from dbally.llms.litellm import LiteLLM
 from dbally.utils.gradio_adapter import GradioAdapter
+from examples.freeform import MyText2SqlView
 from sandbox.quickstart2 import CandidateView, engine, Candidate
 
 dotenv.load_dotenv()
@@ -26,13 +29,21 @@ country_similarity = SimilarityIndex(
     ),
 )
 
+freeFormEngine = sqlalchemy.create_engine("sqlite:///:memory:")
+
 
 async def main():
     llm = LiteLLM(model_name="gpt-3.5-turbo")
+
+    with freeFormEngine.connect() as connection:
+        for table_config in MyText2SqlView(engine).get_tables():
+            connection.execute(sqlalchemy.text(table_config.ddl))
+
     collection = dbally.create_collection("recruitment", llm, event_handlers=[CLIEventHandler()])
     collection.add(CandidateView, lambda: CandidateView(engine))
+    collection.add(MyText2SqlView, lambda: MyText2SqlView(engine))
     gradio_adapter = GradioAdapter(similarity_store=country_similarity, engine=engine)
-    gradio_interface = gradio_adapter.create_interface(collection)
+    gradio_interface = await gradio_adapter.create_interface(collection)
     gradio_interface.launch()
 
 
--- src/dbally/utils/gradio_adapter.py
+++ src/dbally/utils/gradio_adapter.py
@@ -1,15 +1,24 @@
-from typing import Optional, Tuple
+import sys
+from typing import Optional, Tuple, Dict
 
 import gradio
 import pandas as pd
 
+from dbally import BaseStructuredView
 from dbally.collection import Collection
 from dbally.similarity import SimilarityIndex
 from dbally.utils.errors import UnsupportedQueryError
+from dbally.utils.gradio_log_redirect import Logger
+
+
+sys.stdout = Logger("console.log")
 
 
 class GradioAdapter:
     """A class to adapt Gradio interface with a similarity store and data operations."""
+
+    SQL_RESULT = "sql"
+    PANDAS_RESULT = "filter_mask"
 
     def __init__(self, similarity_store: SimilarityIndex = None, engine=None):
         """Initializes the GradioAdapter with an optional similarity store.
@@ -20,27 +29,9 @@ class GradioAdapter:
         self.collection = None
         self.similarity_store = similarity_store
         self.loaded_dataframe = None
-        self.selected_view_name = None
-        self.engine = engine
+        sys.stdout.flush()
 
-    async def load_data(self, input_dataframe: pd.DataFrame) -> str:
-        """Loads data into the adapter from a given DataFrame.
-
-        Args:
-            input_dataframe: The DataFrame to load.
-
-        Returns:
-            A message indicating the data has been loaded.
-        """
-        self.loaded_dataframe = input_dataframe
-        # selected_view = self.collection.get(self.selected_view_name)
-        # table_view = selected_view._select.froms[0]
-        # print(type(selected_view))
-        # self.loaded_dataframe.to_sql(name=table_view, con=self.engine, if_exists="replace")
-
-        return "Frame data loaded."
-
-    async def load_selected_data(self, selected_view_name: str) -> Tuple[pd.DataFrame, str]:
+    async def ui_load_preview_data(self, selected_view_name: str) -> Tuple[pd.DataFrame, str, None, None, None, None]:
         """Loads selected view data into the adapter.
 
         Args:
@@ -49,19 +40,26 @@ class GradioAdapter:
         Returns:
             A tuple containing the loaded DataFrame and a message indicating the view data has been loaded.
         """
-        self.selected_view_name = selected_view_name
+        preview_dataframe, load_status_text = self.load_preview_data(selected_view_name)
+        return preview_dataframe, load_status_text, None, None, None, None
+
+    def load_preview_data(self, selected_view_name: str):
         selected_view = self.collection.get(selected_view_name)
-        selected_view_results = selected_view.execute()
-        self.loaded_dataframe = pd.DataFrame.from_records(selected_view_results.results)
+        text_to_display = "No data preview available"
+        if issubclass(type(selected_view), BaseStructuredView):
+            selected_view_results = selected_view.execute()
+            preview_dataframe = pd.DataFrame.from_records(selected_view_results.results)
+            text_to_display = "Data preview loaded"
+        else:
+            preview_dataframe = pd.DataFrame()
 
-        # self.loaded_dataframe.to_sql(table,   con=selected_view_name._sqlalchemy_engine, if_exists='append')
-        return self.loaded_dataframe, f"{selected_view_name} data loaded."
+        return preview_dataframe, text_to_display
 
-    async def execute_query(self, query: str) -> Tuple[str, Optional[pd.DataFrame]]:
+    async def ui_ask_query(self, question_query: str) -> Tuple[Dict, Optional[pd.DataFrame], str]:
         """Executes a query against the collection.
 
         Args:
-            query: The question to ask.
+            question_query: The question to ask.
 
         Returns:
             A tuple containing the generated SQL (str) and the resulting DataFrame (pd.DataFrame).
@@ -70,13 +68,27 @@ class GradioAdapter:
         try:
             if self.similarity_store:
                 await self.similarity_store.update()
-            execution_result = await self.collection.ask(query)
-            result = execution_result.context.get("sql"), pd.DataFrame.from_records(execution_result.results)
-        except UnsupportedQueryError:
-            result = "Unsupported query", None
-        return result
 
-    def create_interface(self, user_collection: Collection) -> Optional[gradio.Interface]:
+            execution_result = await self.collection.ask(question=question_query)
+            if self.SQL_RESULT in execution_result.context:
+                generated_query = execution_result.context.get(self.SQL_RESULT)
+            elif self.PANDAS_RESULT in execution_result.context:
+                generated_query = execution_result.context.get(self.PANDAS_RESULT)
+            else:
+                generated_query = "Unsupported generated query"
+
+            data = pd.DataFrame.from_records(execution_result.results)
+        except UnsupportedQueryError:
+            generated_query = {"Query": "unsupported"}
+            data = pd.DataFrame()
+        finally:
+            sys.stdout.flush()
+            with open("output.log", "r") as f:
+                log = f.read()
+
+        return generated_query, data, log
+
+    async def create_interface(self, user_collection: Collection) -> Optional[gradio.Interface]:
         """Creates a Gradio interface for the provided user collection.
 
         Args:
@@ -85,32 +97,53 @@ class GradioAdapter:
         Returns:
             The created Gradio interface, or None if no views are available in the collection.
         """
-        view_list = user_collection.list()
+        self.collection = user_collection
+        view_list = [*user_collection.list()]
+        print(view_list[0])
+        if view_list:
+            default_selected_view_name = view_list[0]
+        else:
+            raise ValueError("No view to display")
+
         if not view_list:
             print("There is no data to be loaded")
             return None
 
-        self.collection = user_collection
-
         with gradio.Blocks() as demo:
             with gradio.Row():
                 with gradio.Column():
-                    view_dropdown = gradio.Dropdown(label="Available views", choices=view_list)
-                    load_info = gradio.Label(value="No data loaded.")
+                    view_dropdown = gradio.Dropdown(
+                        label="Data View preview", choices=view_list, value=default_selected_view_name
+                    )
                 with gradio.Column():
-                    loaded_data_frame = gradio.Dataframe(interactive=True, col_count=(4, "Fixed"))
-                    load_data_button = gradio.Button("Load new data")
+                    data_preview_frame, data_preview_status = self.load_preview_data(view_list[0])
+
+                    data_preview_info = gradio.Text(label="Data preview", value=data_preview_status)
+                    loaded_data_frame = gradio.Dataframe(data_preview_frame)
+
             with gradio.Row():
                 query = gradio.Text(label="Ask question")
                 query_button = gradio.Button("Proceed")
             with gradio.Row():
-                query_sql_result = gradio.Text(label="Generated SQL")
+                query_sql_result = gradio.Text(label="Generated query context")
                 query_result_frame = gradio.Dataframe(interactive=False)
+            with gradio.Row():
+                log_console = gradio.Code(label="Logs")
 
             view_dropdown.change(
-                fn=self.load_selected_data, inputs=view_dropdown, outputs=[loaded_data_frame, load_info]
+                fn=self.ui_load_preview_data,
+                inputs=view_dropdown,
+                outputs=[
+                    loaded_data_frame,
+                    data_preview_info,
+                    query,
+                    query_sql_result,
+                    query_result_frame,
+                    log_console,
+                ],
             )
-            load_data_button.click(fn=self.load_data, inputs=loaded_data_frame, outputs=load_info)
-            query_button.click(fn=self.execute_query, inputs=[query], outputs=[query_sql_result, query_result_frame])
+            query_button.click(
+                fn=self.ui_ask_query, inputs=[query], outputs=[query_sql_result, query_result_frame, log_console]
+            )
 
         return demo
