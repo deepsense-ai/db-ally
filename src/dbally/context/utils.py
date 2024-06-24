a87e8e23e1d28ca1fbf6faa061555bab15666f80
Jakub Cierocki <jakub.cierocki@deepsense.ai>
2024-06-24 19:03:02 +0200

    reworked type hint info extraction; extended functionality to also return List[MethodParamWithTyping] required to construct ExposedFunction dataclass

--- src/dbally/context/utils.py
+++ src/dbally/context/utils.py
@@ -1,29 +1,51 @@
 import typing
 
-from .context import BaseCallerContext
+from inspect import isclass
+
+from dbally.context.context import BaseCallerContext
+from dbally.views.exposed_functions import MethodParamWithTyping
 
 
-def _extract_filter_context(filter_: typing.Callable) -> typing.Optional[typing.Type[BaseCallerContext]]:
+def _extract_params_and_context(
+    filter_method_: typing.Callable, hidden_args: typing.List[str]
+) -> typing.Tuple[
+    typing.List[MethodParamWithTyping],
+    typing.Optional[typing.Type[BaseCallerContext]]
+]:
     """
-    Extracts a SINGLE caller's context given a StructuredView filter.
+    Processes the MethodsBaseView filter method signauture to extract the args and type hints in the desired format. Context claases are getting excluded the returned MethodParamWithTyping list. Only the first BaseCallerContext class is returned.
 
     Args:
-        filter_: MethodsBaseView filter method (annotated with @decorators.view_filter() decorator)
+        filter_method_: MethodsBaseView filter method (annotated with @decorators.view_filter() decorator)
 
     Returns:
-        A class inheriting from BaseCallerContext. If not context is given among type hints, value None.
+        A tuple. The first field contains the list of arguments, each encapsulated as MethodParamWithTyping. The 2nd is the BaseCallerContext subclass provided for this filter, or None if no context specified.
     """
 
-    for type_ in typing.get_type_hints(filter_).values():
-        if not isinstance(type_, typing.Union) or not hasattr(type_, '__args__'):
-            # in the 1st condition here we assume that no user you cannot make filter value context-dependent without providing a base type for it
-            # the 2nd condition catches situations when user provides a type hint like typing.Union or typing.Union[int] (single part-clas); in both those cases it should be typing.Union to throw an error, although Python for now accepts this syntax,,,
-            continue
+    params = []
+    context = None
+    # TODO confirm whether to use typing.get_type_hints(method) or method.__annotations__
+    for name_, type_ in typing.get_type_hints(filter_method_).items():
+        if isclass(type_) and issubclass(type_, BaseCallerContext):
+            # this is the case when user provides a context but no other type hint for a specifc arg
+            # TODO confirm whether this case should be supported
+            context_ = type_
+            type_ = typing.Any
+        elif typing.get_origin(type_) is typing.Union:
+            union_subtypes: typing.List[typing.Type] = []
+            for subtype_ in typing.get_args(type_): # type: ignore
+                # TODO add custom error for the situation when user provides more than two contexts for a single filter
+                # for now we extract only the first context
+                if isclass(subtype_) and issubclass(subtype_, BaseCallerContext):
+                    if context is None:
+                        context = subtype_
+                else:
+                    union_subtypes.append(subtype_)
+            if union_subtypes:
+                type_ = typing.Union[tuple(union_subtypes)] # type: ignore
+            else:
+                type_ = typing.Any  # this ELSE handles the situation when the user provided an typing.Union bare type hint, without specyfing any args. In that case, typing.get_args() returns an empty tuple. Unfortunately, Python does not treat it as an error...
 
-        for union_part_type in type_.__args__: # pyright: ignore
-            # TODO add custom error for the situation when user provides more than two contexts for a single filter
-            # for now we extract only the first context
-            if issubclass(union_part_type, BaseCallerContext):
-                return union_part_type
+        params.append(MethodParamWithTyping(name_, type_))
 
-    return None
+    return params, context
