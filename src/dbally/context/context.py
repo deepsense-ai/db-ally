c479c5065b8beb716c5be4a110df2ed431e1638c
Jakub Cierocki <jakub.cierocki@deepsense.ai>
2024-06-28 16:34:35 +0100

    useful classmethods for context-related operations

--- src/dbally/context/context.py
+++ src/dbally/context/context.py
@@ -1,2 +1,27 @@
+import ast
+
+from typing import List, Optional
+from typing_extensions import Self
+from dataclasses import dataclass
+
+from dbally.context.exceptions import ContextNotAvailableError
+
+
+@dataclass
 class BaseCallerContext:
-    pass
+    """
+    Base class for contexts that are used to pass additional knowledge about the caller environment to the filters. It is not made abstract for the convinience of IQL parsing.
+    LLM will always return `BaseCallerContext()` when the context is required and this call will be later substitue by a proper subclass instance selected based on the filter method signature (type hints).
+    """
+
+    @classmethod
+    def select_context(cls, contexts: List[Self]) -> Self:
+        if not contexts:
+            raise ContextNotAvailableError("The LLM detected that the context is required to execute the query and the filter signature allows contextualization while the context was not provided.")
+
+        # we assume here that each element of `contexts` represents a different subclass of BaseCallerContext
+        return next(filter(lambda obj: isinstance(obj, cls), contexts))
+
+    @classmethod
+    def is_context_call(cls, node: ast.expr) -> bool:
+        return isinstance(node, ast.Call) and isinstance(node.func, ast.Name) and node.func.id == cls.__name__
