f432b618cf65e32aace5000bf5047c385b8cb362
Lukasz Karlowski <lukasz.karlowski@deepsense.ai>
2024-07-04 16:20:45 +0200

    chained fallback

--- docs/concepts/collections.md
+++ docs/concepts/collections.md
@@ -25,6 +25,31 @@ my_collection.ask("Find me Italian recipes for soups")
 
 In this scenario, the LLM first determines the most suitable view to address the query, and then that view is used to pull the relevant data.
 
+Sometimes the selected view may not be able to answer the question and will raise an error. In such situations, the fallback collections can be used.
+```python
+    llm = LiteLLM(model_name="gpt-3.5-turbo")
+    user_collection = dbally.create_collection("candidates", llm)
+    user_collection.add(CandidateView, lambda: CandidateView(candidate_view_with_similarity_store.engine))
+    user_collection.add(SampleText2SQLViewCyphers, lambda: SampleText2SQLViewCyphers(create_freeform_memory_engine()))
+    user_collection.add(CandidateView, lambda: (candidate_view_with_similarity_store.engine))
+
+    fallback_collection = dbally.create_collection("freeform candidates", llm)
+    fallback_collection.add(CandidateFreeformView, lambda: CandidateFreeformView(candidates_freeform.engine))
+    user_collection.set_fallback(fallback_collection)
+```
+The fallback collection process the same question with declared set of views. The fallback collection could be chained.
+
+```python
+    second_fallback_collection = dbally.create_collection("recruitment", llm)
+    second_fallback_collection.add(RecruitmentView, lambda: RecruitmentView(recruiting_engine))
+
+    fallback_collection.set_fallback(second_fallback_collection)
+
+```
+
+
+
+
 !!! info
     The result of a query is an [`ExecutionResult`][dbally.collection.results.ExecutionResult] object, which contains the data fetched by the view. It contains a `results` attribute that holds the actual data, structured as a list of dictionaries. The exact structure of these dictionaries depends on the view that was used to fetch the data, which can be obtained by looking at the `view_name` attribute of the `ExecutionResult` object.
 
--- examples/visualize_fallback_code.py
+++ examples/visualize_fallback_code.py
@@ -5,6 +5,8 @@ from recruiting import candidate_view_with_similarity_store, candidates_freeform
 from recruiting.candidate_view_with_similarity_store import CandidateView
 from recruiting.candidates_freeform import CandidateFreeformView
 from recruiting.cypher_text2sql_view import SampleText2SQLViewCyphers, create_freeform_memory_engine
+from recruiting.db import ENGINE as recruiting_engine
+from recruiting.views import RecruitmentView
 
 import dbally
 from dbally.gradio import create_gradio_interface
@@ -13,13 +15,20 @@ from dbally.llms.litellm import LiteLLM
 
 async def main():
     llm = LiteLLM(model_name="gpt-3.5-turbo")
-    collection1 = dbally.create_collection("candidates", llm)
-    collection2 = dbally.create_collection("freeform candidates", llm)
-    collection1.add(CandidateView, lambda: CandidateView(candidate_view_with_similarity_store.engine))
-    collection1.add(SampleText2SQLViewCyphers, lambda: SampleText2SQLViewCyphers(create_freeform_memory_engine()))
-    collection2.add(CandidateFreeformView, lambda: CandidateFreeformView(candidates_freeform.engine))
-    collection1.set_fallback(collection2)
-    gradio_interface = await create_gradio_interface(user_collection=collection1)
+    user_collection = dbally.create_collection("candidates", llm)
+    user_collection.add(CandidateView, lambda: CandidateView(candidate_view_with_similarity_store.engine))
+    user_collection.add(SampleText2SQLViewCyphers, lambda: SampleText2SQLViewCyphers(create_freeform_memory_engine()))
+    user_collection.add(CandidateView, lambda: (candidate_view_with_similarity_store.engine))
+
+    fallback_collection = dbally.create_collection("freeform candidates", llm)
+    fallback_collection.add(CandidateFreeformView, lambda: CandidateFreeformView(candidates_freeform.engine))
+
+    second_fallback_collection = dbally.create_collection("recruitment", llm)
+    second_fallback_collection.add(RecruitmentView, lambda: RecruitmentView(recruiting_engine))
+
+    user_collection.set_fallback(fallback_collection).set_fallback(second_fallback_collection)
+
+    gradio_interface = await create_gradio_interface(user_collection=user_collection)
     gradio_interface.launch()
 
 
