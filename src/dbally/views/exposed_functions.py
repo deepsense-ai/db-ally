1294a9ca94e69fab36182ed943db275c7ff0d97d
Jakub Cierocki <jakub.cierocki@deepsense.ai>
2024-07-09 17:30:54 +0200

    type hint parsing changes: SomeCustomContext -> AskerContext; Union[a, b] -> a | b; removed typing & typing_extensions module name prefixes

--- src/dbally/views/exposed_functions.py
+++ src/dbally/views/exposed_functions.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from inspect import isclass
 from typing import _GenericAlias  # type: ignore
-from typing import Optional, Sequence, Type, Union
+from typing import Optional, Sequence, Type, Union, Generator
 
 import typing_extensions as type_ext
 
@@ -22,6 +22,11 @@ def parse_param_type(param_type: Union[type, _GenericAlias, str]) -> str:
 
     # TODO consider using hasattr() to ensure correctness of the IF's below
     if isclass(param_type):
+        if issubclass(param_type, BaseCallerContext):
+            # this mechanism ensures the LLM will be able to notice the relation between
+            # the keyword-call specified in the prompt and the filter method signatures
+            return BaseCallerContext._alias
+
         return param_type.__name__
 
     # typing.Literal['aaa', 'bbb'] edge case handler
@@ -31,16 +36,22 @@ def parse_param_type(param_type: Union[type, _GenericAlias, str]) -> str:
         return f"'{param_type}'"
 
     if param_type.__module__ in ["typing", "typing_extensions"]:
-        type_args = type_ext.get_args(param_type)
-        if type_args:
-            param_name = param_type._name  # pylint: disable=protected-access
-            if param_name is None:
-                # workaround for typing.Literal, because: `typing.Literal['aaa', 'bbb']._name is None`
-                # but at the same time: `type_ext.get_origin(typing.Literal['aaa', 'bbb'])._name == "Literal"`
-                param_name = type_ext.get_origin(param_type)._name  # pylint: disable=protected-access
+        param_name = param_type._name  # pylint: disable=protected-access
+        if param_name is None:
+            # workaround for typing.Literal, because: `typing.Literal['aaa', 'bbb']._name is None`
+            # but at the same time: `type_ext.get_origin(typing.Literal['aaa', 'bbb'])._name == "Literal"`
+            param_name = type_ext.get_origin(param_type)._name  # pylint: disable=protected-access
 
-            args_str_repr = ", ".join(parse_param_type(arg) for arg in type_args)
-            return f"{param_name}[{args_str_repr}]"
+        type_args = type_ext.get_args(param_type)
+        if not type_args:
+            return param_name
+
+        parsed_args: Generator[str] = (parse_param_type(arg) for arg in type_args)
+        if type_ext.get_origin(param_type) is Union:
+            return " | ".join(parsed_args)
+
+        parsed_args_concatanated = ", ".join(parsed_args)
+        return f"{param_name}[{parsed_args_concatanated}]"
 
     return str(param_type)
 
