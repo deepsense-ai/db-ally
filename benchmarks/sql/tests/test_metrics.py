1f0214a8271304324817afd85805f296f60ba4b9
Michał Pstrąg <michal.pstrag@icloud.com>
2024-07-24 10:56:02 +0200

    add tests for ex

--- benchmarks/sql/tests/test_metrics.py
+++ benchmarks/sql/tests/test_metrics.py
@@ -1,9 +1,21 @@
+from dataclasses import dataclass
 from typing import List
+from unittest.mock import MagicMock
 
 import pytest
 
-from benchmarks.sql.bench.metrics.iql import ExactMatchIQL, ValidIQL
-from benchmarks.sql.bench.pipeline import EvaluationResult, ExecutionResult
+from benchmarks.sql.bench.metrics import ExactMatchIQL, ExecutionAccuracy, ValidIQL
+from benchmarks.sql.bench.pipelines import EvaluationResult, ExecutionResult
+
+
+@dataclass
+class MockDataConfig:
+    db_url: str = "sqlite:///:memory:"
+
+
+@dataclass
+class MockConfig:
+    data: MockDataConfig = MockDataConfig()
 
 
 @pytest.fixture
@@ -34,9 +46,36 @@ def evaluation_results() -> List[EvaluationResult]:
 
 def test_exact_match_iql(evaluation_results: List[EvaluationResult]) -> None:
     metric = ExactMatchIQL()
-    assert metric.compute(evaluation_results) == 0.5
+    scores = metric.compute(evaluation_results)
+    assert scores["EM_IQL"] == 0.5
 
 
-def test_valid_iql(evaluation_results):
+def test_valid_iql(evaluation_results) -> None:
     metric = ValidIQL()
-    assert metric.compute(evaluation_results) == 1
+    scores = metric.compute(evaluation_results)
+    assert scores["VAL_IQL"] == 1.0
+
+
+@pytest.mark.parametrize(
+    "acc, avg_times, expected_ex, expected_ves",
+    [
+        ([True, False, True, True], [1.2, 1.2, 12.2, 12.2, 13.2, 13.2, 232.1, 232.1], 0.75, 0.75),
+        ([True, True, True, True], [1.2, 1.2, 12.2, 12.2, 13.2, 13.2, 232.1, 232.1], 1.0, 1.0),
+        ([False, False, False, False], [1.2, 1.2, 12.2, 12.2, 13.2, 13.2, 232.1, 232.1], 0.0, 0.0),
+        ([True, False, True, True], [1.2, 3.2, 12.2, 15.2, 13.2, 17.2, 232.1, 287.1], 0.75, 0.5960767767585372),
+        ([True, False, True, True], [3.2, 1.2, 15.2, 12.2, 17.2, 13.2, 287.1, 232.1], 0.75, 0.9726740826467557),
+    ],
+)
+def test_execution_accuracy(
+    evaluation_results: List[EvaluationResult],
+    acc: List[bool],
+    avg_times: List[float],
+    expected_ex: float,
+    expected_ves: float,
+) -> None:
+    metric = ExecutionAccuracy(MockConfig())
+    metric._execution_accuracy = MagicMock(side_effect=acc)
+    metric._avarage_execution_time = MagicMock(side_effect=avg_times)
+    scores = metric.compute(evaluation_results)
+    assert scores["EX"] == expected_ex
+    assert scores["VES"] == expected_ves
