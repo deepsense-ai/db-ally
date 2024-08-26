80c932c01ac7436ee333f58c4b2ee1f31af72191
Michał Pstrąg <michal.pstrag@icloud.com>
2024-08-26 12:07:00 +0200

    correct prompt

--- extra/prompt_tuning/tuning/signatures/iql.py
+++ extra/prompt_tuning/tuning/signatures/iql.py
@@ -22,9 +22,8 @@ class CheckQuestionFiltering(Signature):
 
 class CheckQuestionAggregation(Signature):
     """
-    Given a question, determine whether the answer requires initial data filtering in order to compute it.
-    Initial data filtering is a process in which the result set is reduced to only include the rows that
-    meet certain criteria specified in the question.
+    Given a question, determine whether the answer requires data aggregation in order to compute it.
+    Data aggregation is a process in which we calculate a single values for a group of rows in the result set.
     """
 
     question = InputField(
@@ -33,7 +32,6 @@ class CheckQuestionAggregation(Signature):
     decision = OutputField(
         prefix="Decision: ",
         desc=(
-            "indicates whether the answer to the question requires initial data filtering. "
-            "(Respond with True or False)"
+            "indicates whether the answer to the question requires data aggregation. " "(Respond with True or False)"
         ),
     )
