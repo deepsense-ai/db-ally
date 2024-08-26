2765afa8edb1cb931c64cc6f447d00332a65b263
Michał Pstrąg <michal.pstrag@icloud.com>
2024-08-26 10:16:29 +0200

    update benchmark dataset

--- extra/prompt_tuning/tuning/programs/__init__.py
+++ extra/prompt_tuning/tuning/programs/__init__.py
@@ -1,8 +1,16 @@
-from .iql import FilteringAssessorBaseline, FilteringAssessorCoT
+from .iql import AggregationAssessorBaseline, AggregationAssessorCoT, FilteringAssessorBaseline, FilteringAssessorCoT
 
 PROGRAMS = {
     FilteringAssessorBaseline.__name__: FilteringAssessorBaseline,
     FilteringAssessorCoT.__name__: FilteringAssessorCoT,
+    AggregationAssessorBaseline.__name__: AggregationAssessorBaseline,
+    AggregationAssessorCoT.__name__: AggregationAssessorCoT,
 }
 
-__all__ = ["PROGRAMS", "FilteringAssessorBaseline", "FilteringAssessorCoT"]
+__all__ = [
+    "PROGRAMS",
+    "AggregationAssessorBaseline",
+    "AggregationAssessorCoT",
+    "FilteringAssessorBaseline",
+    "FilteringAssessorCoT",
+]
