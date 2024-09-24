61e82b7d9be194694993c1e537abd8e0da0514ab
Michał Pstrąg <michal.pstrag@icloud.com>
2024-09-24 23:18:24 +0000

    feat(extra): aggregtion prompts tuning (#83)

--- extra/prompt_tuning/tuning/metrics/__init__.py
+++ extra/prompt_tuning/tuning/metrics/__init__.py
@@ -1,3 +1,3 @@
-from .iql import filtering_assess_acc
+from .iql import aggregation_assess_acc, filtering_assess_acc
 
-__all__ = ["filtering_assess_acc"]
+__all__ = ["aggregation_assess_acc", "filtering_assess_acc"]
--- extra/prompt_tuning/tuning/programs/iql.py
+++ extra/prompt_tuning/tuning/programs/iql.py
@@ -1,16 +1,18 @@
-from dspy import ChainOfThought, Module, Predict, Prediction
+from typing import Type
 
-from ..signatures.iql import CheckQuestionFiltering
+from dspy import ChainOfThought, ChainOfThoughtWithHint, Module, Predict, Prediction
+
+from ..signatures.iql import AggregationAssessor, FilteringAssessor
 
 
-class FilteringAssessorBaseline(Module):
+class FilteringAssessorPredict(Module):
     """
     Program that assesses whether a question requires filtering.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, signature: Type[FilteringAssessor]) -> None:
         super().__init__()
-        self.decide = Predict(CheckQuestionFiltering)
+        self.decide = Predict(signature)
 
     def forward(self, question: str) -> Prediction:
         """
@@ -31,9 +33,9 @@ class FilteringAssessorCoT(Module):
     Program that assesses whether a question requires filtering.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, signature: Type[FilteringAssessor]) -> None:
         super().__init__()
-        self.decide = ChainOfThought(CheckQuestionFiltering)
+        self.decide = ChainOfThought(signature)
 
     def forward(self, question: str) -> Prediction:
         """
@@ -46,4 +48,102 @@ class FilteringAssessorCoT(Module):
             The prediction.
         """
         decision = self.decide(question=question).decision
+        return Prediction(decision=decision.lower() == "true")
+
+
+class FilteringAssessorCoTH(Module):
+    """
+    Program that assesses whether a question requires filtering.
+    """
+
+    def __init__(self, signature: Type[FilteringAssessor]) -> None:
+        super().__init__()
+        self.decide = ChainOfThoughtWithHint(signature)
+
+    def forward(self, question: str) -> Prediction:
+        """
+        Assess whether a question requires filtering.
+
+        Args:
+            question: The question to assess.
+
+        Returns:
+            The prediction.
+        """
+        decision = self.decide(
+            question=question,
+            hint="Look for words indicating data specific features.",
+        ).decision
+        return Prediction(decision=decision.lower() == "true")
+
+
+class AggregationAssessorPredict(Module):
+    """
+    Program that assesses whether a question requires aggregation.
+    """
+
+    def __init__(self, signature: Type[AggregationAssessor]) -> None:
+        super().__init__()
+        self.decide = Predict(signature)
+
+    def forward(self, question: str) -> Prediction:
+        """
+        Assess whether a question requires aggregation.
+
+        Args:
+            question: The question to assess.
+
+        Returns:
+            The prediction.
+        """
+        decision = self.decide(question=question).decision
+        return Prediction(decision=decision.lower() == "true")
+
+
+class AggregationAssessorCoT(Module):
+    """
+    Program that assesses whether a question requires aggregation.
+    """
+
+    def __init__(self, signature: Type[AggregationAssessor]) -> None:
+        super().__init__()
+        self.decide = ChainOfThought(signature)
+
+    def forward(self, question: str) -> Prediction:
+        """
+        Assess whether a question requires aggregation.
+
+        Args:
+            question: The question to assess.
+
+        Returns:
+            The prediction.
+        """
+        decision = self.decide(question=question).decision
+        return Prediction(decision=decision.lower() == "true")
+
+
+class AggregationAssessorCoTH(Module):
+    """
+    Program that assesses whether a question requires aggregation.
+    """
+
+    def __init__(self, signature: Type[AggregationAssessor]) -> None:
+        super().__init__()
+        self.decide = ChainOfThoughtWithHint(signature)
+
+    def forward(self, question: str) -> Prediction:
+        """
+        Assess whether a question requires aggregation.
+
+        Args:
+            question: The question to assess.
+
+        Returns:
+            The prediction.
+        """
+        decision = self.decide(
+            question=question,
+            hint="Look for words indicating aggregation functions.",
+        ).decision
         return Prediction(decision=decision.lower() == "true")
