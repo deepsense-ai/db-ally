8b1c5284ae8863eeb0837694ceeb9d3e2c195533
Ludwik Trammer <ludwik.trammer@deepsense.ai>
2025-04-16 14:47:52 +0200

    Fix testing

--- tests/integration/test_llm_options.py
+++ tests/integration/test_llm_options.py
@@ -1,10 +1,24 @@
+from typing import List
 from unittest.mock import ANY, AsyncMock, call, patch
 
 import pytest
 
 from dbally import create_collection
-from tests.unit.mocks import MockLLM, MockLLMOptions
-from tests.unit.test_collection import MockView1, MockView2
+from dbally.views.exposed_functions import ExposedFunction
+from tests.unit.mocks import MockLLM, MockLLMOptions, MockViewBase
+from tests.unit.test_collection import MockView1
+
+
+class MockViewWithFilterAndAggregation(MockViewBase):
+    """
+    Mock view with an example filter and aggregation.
+    """
+
+    def list_filters(self) -> List[ExposedFunction]:
+        return [ExposedFunction("test_filter", "", [])]
+
+    def list_aggregations(self) -> List[ExposedFunction]:
+        return [ExposedFunction("test_aggregation", "", [])]
 
 
 @pytest.mark.asyncio
@@ -14,7 +28,7 @@ async def test_llm_options_propagation():
     expected_options = MockLLMOptions(mock_property1=2, mock_property2="default mock")
 
     llm = MockLLM(default_options=default_options)
-    llm.client.call = AsyncMock(return_value="MockView1")
+    llm.client.call = AsyncMock(return_value="MockViewWithFilterAndAggregation")
 
     collection = create_collection(
         name="test_collection",
@@ -22,7 +36,7 @@ async def test_llm_options_propagation():
     )
     collection.n_retries = 0
     collection.add(MockView1)
-    collection.add(MockView2)
+    collection.add(MockViewWithFilterAndAggregation)
 
     with patch("dbally.iql.IQLQuery.parse", AsyncMock()):
         await collection.ask(
